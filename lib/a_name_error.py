cff683451a3b4e83f6cd21a999055592ed7731c5
Amene <layla_1380@yahoo.com>
2024-08-11 13:51:35 -0500

    errors fixed

--- lib/a_name_error.py
+++ lib/a_name_error.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
-print(hello_world)
+print('hello_world')
+
+# FAILED a_name_error.py contains defined name "hello_world" - NameError: name 'hello_world' is not defined
+# FAILED a_syntax_error.py multiplies two numbers -   File "lib/a_syntax_error.py", line 3
+# FAILED a_type_error.py adds two numbers - TypeError: can only concatenate str (not "int") to str
+# FAILED an_assertion_error.py evaluates two equal values - AssertionError
+# ============================== 4 failed in 0.04s ===============================
+# (python-p3-reading-python-error-messages) layla@laylas-Air python-p3-reading-python-error-messages % 
